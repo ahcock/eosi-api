3bcb97b3c9c57ddde847581eef47bc019d654250
ahcock <srparkgogo@gmail.com>
2024-03-14 15:33:09 +0900

    Refactor dotenv.config execute order

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import app from "./server";
 import * as dotenv from 'dotenv'
-
 dotenv.config()
+
+import app from "./server";
 
 app.listen(3001, () => {
   console.log("http://localhost:3001 에 서버가 돌고 있어요!");
