1cd9ea6b35b0e84f746e78fc8d9a6b1cf8311bbd
ahcock <srparkgogo@gmail.com>
2024-03-15 17:01:08 +0900

    Write util functions for hashing and comparing password

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,9 +1,20 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
 
 type User = {
   id: string;
   username: string;
+};
+
+const comparePassword = (password: string | Buffer, hash: string) => {
+  return bcrypt.compare(password, hash);
+};
+
+const hashPassword = (password: string) => {
+  // 더 안전한 salt 사용을 위해 추후 바꿀 것.
+
+  return bcrypt.hash(password, 5);
 };
 export const createJWT = (user: User) => {
   const token = jwt.sign(
