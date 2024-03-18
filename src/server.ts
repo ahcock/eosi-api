3dce572244ef93d6692d9e730e02c45ef43c1774
ahcock <srparkgogo@gmail.com>
2024-03-18 12:21:15 +0900

    Make signin, signup post route
    - make signup, signin post route
    - add express.json() middleware to parse requst.body

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,15 @@ import express from "express";
 import router from "./routes";
 import morgan from "morgan";
 import { protect } from "./modules/auth";
+import { createNewUser, signin } from "./handlers/user";
 
 const app = express();
+app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/", protect, router);
+app.use("/api", protect, router);
+
+app.post("/user", createNewUser);
+app.post("/signin", signin);
 
 export default app;
