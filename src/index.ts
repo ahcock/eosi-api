ecafa9d6e3c1d382982bb8744c51077054cc4ef2
ahcock <srparkgogo@gmail.com>
2024-03-23 23:15:28 +0900

    Make env config dynamic
    - make env variables dynamic with STAGE flag when running app
    
    - set custom config based on STAGE flag when running app. and able to override old config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
+import config from "./config";
 
 import app from "./server";
 
-app.listen(3001, () => {
-  console.log("http://localhost:3001 에 서버가 돌고 있어요!");
+app.listen(config.port, () => {
+  console.log(`http://localhost:${config.port} 에 서버가 돌고 있어요!`);
 });
